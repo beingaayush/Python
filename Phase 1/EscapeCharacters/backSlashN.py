581c7e38502819b04eaaa525dc7a3e1fddbfe7b9
beingaayush <beingaayushraj@gmail.com>
2026-07-13 15:09:03 +0530

    escape chars - \n & \t

---
+++ Phase 1/EscapeCharacters/backSlashN.py
@@ -0,0 +1,6 @@
+# \n is used for printing next char on the next line
+print("hello\nworld")
+
+# output :
+# hello
+# world
---
+++ Phase 1/EscapeCharacters/backSlashT.py
@@ -0,0 +1,4 @@
+# \t is used to give tab space b/w char/ words
+print("hello\tworld")
+
+# output: hello  world
