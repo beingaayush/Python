60a050b76e41c573a73aed3ce24dd4c1903b3d41
beingaayush <beingaayushraj@gmail.com>
2026-07-13 13:44:45 +0530

    sep parameter

--- Phase 1/Print/SEP_parameter.py
+++ Phase 1/Print/SEP_parameter.py
@@ -1,4 +1,19 @@
 # Default separator " " hota hai.
+
 print("Aayush", "Kumar")
 
 # output - Aayush Kumar
+# -----------------------------------------------------------------
+
+
+# change separator :-
+
+# ex1:
+print("Aayush", "Kumar", sep="-")
+
+# output - Aayush-Kumar
+
+# ex2:
+print(1, 2, 3, 4, 5, sep="->")
+
+# output - 1 -> 2 -> 3 -> 4 -> 5
