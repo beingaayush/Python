ed0f77046b6f1b033c435146d5cd06234f1097b4
beingaayush <beingaayushraj@gmail.com>
2026-07-13 13:54:36 +0530

    var initiated

---
+++ Phase 1/Variables/var.py
@@ -0,0 +1,8 @@
+# Variable in python diff from c++ :-
+# > Python me variable ka type likhna nahi padta.
+# > Python khud detect karta hai.
+# > Isko Dynamic Typing kehte hain.
+
+name = "Aayush"
+age = 22
+cgpa = 7.5
