84d85609ef6cf542b4c78edc5f7a54cc5ed526b5
beingaayush <beingaayushraj@gmail.com>
2026-07-13 15:46:28 +0530

    str to int & float | int  to str and float | float to int

---
+++ Phase 1/TypeCasting/code.py
@@ -0,0 +1,45 @@
+# TypeCasting - Data Type convert krna
+
+# string -> int
+age = int(input("Enter age: "))
+print(age)
+print(type(age)) # int
+
+# Now we can do this also
+age = int(input("Enter age: "))
+print(age + 10)
+
+# --------------------------------------------------------------------------------------------
+
+
+# string -> float
+cgpa = float(input("enter ur gpa: "))
+print(cgpa)
+
+# --------------------------------------------------------------------------------------------
+
+
+# integer -> string
+age = 22
+text = str(age)
+print(type(text)) #string
+
+
+# --------------------------------------------------------------------------------------------
+
+
+# integer -> float
+x = 10
+y = float(x)
+
+print(y)  # 10.0
+
+
+# --------------------------------------------------------------------------------------------
+
+
+# float -> integer
+x = 9.8
+y = int(x)
+
+print(y)  # 9
